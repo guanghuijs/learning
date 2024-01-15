c49cc6dc04a0cd286a263220648738ee3cd236b0
吴光辉 <995980020@qq.com>
2024-01-15 17:36:31 +0800

    fix(router): 路由方法抽离

--- vue/vue3/src/router/index.ts
+++ vue/vue3/src/router/index.ts
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import routes from '~pages';
-import { defineComponent, h } from 'vue';
-import { Home } from '@/components/home';
-import { filterRoutes } from '@/router/utils';
+import { getModuleRouter } from '@/router/utils';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,66 +8,11 @@ const router = createRouter({
       path: '/',
       redirect: '/vue',
     },
-    {
-      path: '/vue',
-      redirect: '/vue/1.template',
-      name: 'vue',
-      component: defineComponent(() => {
-        return () =>
-          h(Home, {
-            routes: filterRoutes(routes, 'vue') as any,
-          });
-      }),
-      children: filterRoutes(routes, 'vue'),
-    },
-    {
-      path: '/demo',
-      redirect: '/demo/grape',
-      name: 'demo',
-      component: defineComponent(() => {
-        return () =>
-          h(Home, {
-            routes: filterRoutes(routes, 'demo') as any,
-          });
-      }),
-      children: filterRoutes(routes, 'demo'),
-    },
-    {
-      path: '/three-js',
-      redirect: '/three-js/amf',
-      name: 'threeJs',
-      component: defineComponent(() => {
-        return () =>
-          h(Home, {
-            routes: filterRoutes(routes, 'three-js') as any,
-          });
-      }),
-      children: filterRoutes(routes, 'three-js'),
-    },
-    {
-      path: '/css',
-      redirect: '/css/1.属性选择器',
-      name: 'css',
-      component: defineComponent(() => {
-        return () =>
-          h(Home, {
-            routes: filterRoutes(routes, 'css') as any,
-          });
-      }),
-      children: filterRoutes(routes, 'css'),
-    },
-    {
-      path: '/es6',
-      redirect: '/es6/1.let',
-      name: 'es6',
-      component: defineComponent(() => {
-        return () =>
-          h(Home, {
-            routes: filterRoutes(routes, 'es6') as any,
-          });
-      }),
-      children: filterRoutes(routes, 'es6'),
-    },
+    getModuleRouter('vue', '1.template'),
+    getModuleRouter('demo', 'grape'),
+    getModuleRouter('three-js', 'amf'),
+    getModuleRouter('css', '1.属性选择器'),
+    getModuleRouter('es6', '1.let'),
   ],
 });
 
--- vue/vue3/src/router/utils.ts
+++ vue/vue3/src/router/utils.ts
@@ -1,4 +1,7 @@
 import type { RouteRecordRaw } from 'vue-router';
+import { defineComponent, h } from 'vue';
+import { Home } from '@/components/home';
+import routes from '~pages';
 
 export function filterRoutes(routes: RouteRecordRaw[], routeType: string): RouteRecordRaw[] {
   return routes
@@ -9,4 +12,24 @@ export function filterRoutes(routes: RouteRecordRaw[], routeType: string): Route
       });
     })
     .sort((a, b) => a.order - b.order);
+}
+
+/**
+ * 获取模块路由,模块中的.vue文件不能出现大写字母,不然识别不到代码
+ * @param modelName 模块名字需要与目录保持一致
+ * @param firstRote 模块首屏路由
+ */
+export function getModuleRouter(modelName: string, firstRote: string) {
+  return {
+    path: `/${modelName}`,
+    redirect: `/${modelName}/${firstRote}`,
+    name: modelName,
+    component: defineComponent(() => {
+      return () =>
+        h(Home, {
+          routes: filterRoutes(routes, modelName) as any,
+        });
+    }),
+    children: filterRoutes(routes, modelName),
+  };
 }
