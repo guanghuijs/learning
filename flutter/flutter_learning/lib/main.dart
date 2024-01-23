57810067fa150edc101a6489b39e746027818e61
吴光辉 <995980020@qq.com>
2024-01-24 00:17:31 +0800

    feat(flutter): demo init

---
+++ flutter/flutter_learning/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './pages/home/index.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter learning',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
+
