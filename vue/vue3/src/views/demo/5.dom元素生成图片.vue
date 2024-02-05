f5520c17df1b72eade4a064026fcac8ff43d2901
吴光辉 <995980020@qq.com>
2024-02-05 17:14:22 +0800

    feat(demo): dom元素生成图片

---
+++ vue/vue3/src/views/demo/5.dom元素生成图片.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+  import html2canvas from 'html2canvas';
+  const targetRef = ref<HTMLElement | null>();
+
+  const url = ref('');
+
+  onMounted(() => {
+    targetRef.value?.addEventListener('mouseover', (e) => {
+      html2canvas(targetRef.value!).then((res) => {
+        url.value = res.toDataURL('image/jpeg', 1.0);
+      });
+    });
+  });
+</script>
+
+<template>
+  <div class="container">
+    <div ref="targetRef" style="background: red" class="target">
+      <div>112</div>
+      <p>337</p>
+      <h1 style="text-align: center; color: blueviolet">999</h1>
+    </div>
+    <img :src="url" alt="" />
+  </div>
+</template>
+
+<style scoped lang="less"></style>
