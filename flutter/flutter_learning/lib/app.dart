93662185a6d1d9ce52d03648daaa097723429238
吴光辉 <995980020@qq.com>
2024-03-10 23:55:56 +0800

    feat(flutter): home

--- flutter/flutter_learning/lib/app.dart
+++ flutter/flutter_learning/lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import './route/index.dart';
-import './widget/nav_item.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -17,18 +16,38 @@ class _App extends State<App> {
         initialRoute: Routes.initialRoute,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          //Scaffold脚手架的背景色
+          scaffoldBackgroundColor: const Color(0xfff2f2f2),
         ),
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('flutter learn'),
-              centerTitle: true,
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.deepPurpleAccent,
-            ),
-            body: ListView.builder(
-                itemCount: Routes.route.length,
-                itemBuilder: (content, index) {
-                  return NavItemB(navItem: Routes.route[index]);
-                })));
+          appBar: AppBar(
+            title: const Text('flutter learn'),
+            centerTitle: true,
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+          // body: ListView.builder(
+          //     itemCount: Routes.route.length,
+          //     itemBuilder: (content, index) {
+          //       return NavItemB(navItem: Routes.route[index]);
+          //     }),
+          body: ListView.builder(
+            itemCount: Routes.route.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  debugPrint('test');
+                  Navigator.of(context).pushNamed(Routes.route[index].path);
+                },
+                child: Container(
+                  color: Colors.white,
+                  margin: const EdgeInsets.only(top: 1),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(Routes.route[index].title),
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
