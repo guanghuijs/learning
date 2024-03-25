c63c940c334c19b8776b376c941dcd5e62699cb9
吴光辉 <995980020@qq.com>
2024-03-26 00:03:20 +0800

    feat(flutter): 表单样式

--- flutter/flutter_learning/lib/pages/demo/表单校验.dart
+++ flutter/flutter_learning/lib/pages/demo/表单校验.dart
@@ -36,140 +36,8 @@ class _FormVerify extends State<FormVerify> {
         key: _formKey,
         child: Container(
           padding: const EdgeInsets.all(10),
-          child: Column(
+          child: ListView(
             children: [
-              Container(
-                margin: const EdgeInsets.only(top: 1),
-                padding: const EdgeInsets.all(10),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        width: 100,
-                        padding: const EdgeInsets.fromLTRB(0, 8, 10, 0),
-                        child: const Text(
-                          '姓名',
-                          textDirection: TextDirection.rtl,
-                        )),
-                    Expanded(
-                      child: TextFormField(
-                        textAlign: TextAlign.right,
-                        decoration: const InputDecoration(
-                            isCollapsed: true,
-                            filled: true,
-                            fillColor: Color(0xfff2f2f2),
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            border: InputBorder.none,
-                            hintText: '请输入'),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return '不能为空';
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 1),
-                color: Colors.white,
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        width: 100,
-                        padding: const EdgeInsets.fromLTRB(0, 8, 10, 0),
-                        child: const Text(
-                          '姓名',
-                          textDirection: TextDirection.rtl,
-                        )),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: InkWell(
-                              onTap: () {
-                                BrnToast.show('点击', context);
-                              },
-                              child: TextFormField(
-                                decoration: const InputDecoration(
-                                    hintTextDirection: TextDirection.rtl,
-                                    enabled: false,
-                                    isCollapsed: true,
-                                    fillColor: Color(0xfff2f2f2),
-                                    contentPadding:
-                                        EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                    border: InputBorder.none,
-                                    hintText: '请输入'),
-                                validator: (value) {
-                                  if (value!.isEmpty) {
-                                    return '不能为空';
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                          const Icon(Icons.keyboard_arrow_right)
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 1),
-                color: Colors.white,
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        padding: const EdgeInsets.fromLTRB(0, 8, 10, 0),
-                        child: const Text('姓名')),
-                    Expanded(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            isCollapsed: true,
-                            fillColor: Color(0xffffffff),
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                            border: InputBorder.none,
-                            hintText: '请输入'),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return '不能为空';
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const CodeBlock(
-                code: '''
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-            filled: true,
-            contentPadding: EdgeInsets.all(10),
-            border: InputBorder.none,
-            hintText: '请输入'),
-                validator: (value) {
-          if (value!.isEmpty) {
-            return '不能为空';
-          }
-                },
-              ),
-            ),
-          ''',
-                title: '关键代码',
-              ),
               Container(
                 margin: const EdgeInsets.only(top: 1),
                 color: Colors.white,
@@ -188,6 +56,29 @@ class _FormVerify extends State<FormVerify> {
                   },
                 ),
               ),
+              const CodeBlock(
+                code: '''
+  Container(
+    margin: const EdgeInsets.only(top: 1),
+    color: Colors.white,
+    padding: const EdgeInsets.all(10),
+    child: TextFormField(
+      decoration: const InputDecoration(
+          filled: true,
+          contentPadding: EdgeInsets.all(0),
+          border: InputBorder.none,
+          hintText: '请输入',
+          hintStyle: TextStyle(height: 0)),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return '不能为空';
+        }
+      },
+    ),
+  ),
+          ''',
+                title: '关键代码',
+              ),
               Container(
                 color: Colors.white,
                 padding: const EdgeInsets.all(10),
@@ -204,6 +95,27 @@ class _FormVerify extends State<FormVerify> {
                   },
                 ),
               ),
+              const CodeBlock(
+                code: '''
+  Container(
+    color: Colors.white,
+    padding: const EdgeInsets.all(10),
+    margin: const EdgeInsets.only(top: 1),
+    child: TextFormField(
+      decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          border: InputBorder.none,
+          hintText: '请输入'),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return '不能为空';
+        }
+      },
+    ),
+  ),
+          ''',
+                title: '关键代码',
+              ),
               Container(
                 margin: const EdgeInsets.only(top: 1),
                 padding: const EdgeInsets.all(10),
@@ -211,28 +123,33 @@ class _FormVerify extends State<FormVerify> {
                   color: Colors.white,
                 ),
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                        width: 100,
-                        height: 45,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            '姓名',
-                            textDirection: TextDirection.rtl,
-                          ),
-                        )),
                     Expanded(
                       child: TextFormField(
                         textAlign: TextAlign.right,
-                        decoration: const InputDecoration(
-                            // isCollapsed: true,
+                        style: const TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                            prefixIcon: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              width: 100,
+                              child: const Row(
+                                // mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '姓名',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ),
                             filled: true,
-                            fillColor: Color(0xfff2f2f2),
-                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            fillColor: const Color(0xfff2f2f2),
                             border: InputBorder.none,
-                            hintText: '请输入'),
+                            hintText: '请输入',
+                            hintStyle: const TextStyle(
+                                fontSize: 20, color: Color(0xff999999))),
                         validator: (value) {
                           if (value!.isEmpty) {
                             return '不能为空';
@@ -244,58 +161,176 @@ class _FormVerify extends State<FormVerify> {
                   ],
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 1),
-                color: Colors.white,
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        width: 100,
-                        height: 45,
-                        color: Colors.red,
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              '设备名称',
-                              textDirection: TextDirection.rtl,
-                            ),
-                          ],
-                        )),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: InkWell(
-                              onTap: () {
-                                BrnToast.show('点击', context);
-                              },
-                              child: TextFormField(
-                                decoration: const InputDecoration(
-                                    hintTextDirection: TextDirection.ltr,
-                                    enabled: false,
-                                    isCollapsed: true,
-                                    fillColor: Color(0xfff2f2f2),
-                                    // contentPadding:
-                                    //     EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                    border: InputBorder.none,
-                                    hintText: '请输入'),
-                                validator: (value) {
-                                  if (value!.isEmpty) {
-                                    return '不能为空';
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                          const Icon(Icons.keyboard_arrow_right)
-                        ],
+              const CodeBlock(
+                code: '''
+  Container(
+    margin: const EdgeInsets.only(top: 1),
+    padding: const EdgeInsets.all(10),
+    decoration: const BoxDecoration(
+      color: Colors.white,
+    ),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: TextFormField(
+            textAlign: TextAlign.right,
+            style: const TextStyle(fontSize: 20),
+            decoration: InputDecoration(
+                prefixIcon: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10),
+                  width: 100,
+                  child: const Row(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '姓名',
+                        style: TextStyle(fontSize: 20),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+                filled: true,
+                fillColor: const Color(0xfff2f2f2),
+                border: InputBorder.none,
+                hintText: '请输入',
+                hintStyle: const TextStyle(
+                    fontSize: 20, color: Color(0xff999999))),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return '不能为空';
+              }
+              return null;
+            },
+          ),
+        ),
+      ],
+    ),
+  ),
+          ''',
+                title: '关键代码',
+              ),
+              InkWell(
+                onTap: () {
+                  BrnToast.show('哈哈哈哈', context);
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 1),
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          textAlign: TextAlign.right,
+                          enabled: false,
+                          style: const TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                              prefixIcon: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                width: 100,
+                                child: const Row(
+                                  // mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      '姓名',
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              suffixIcon: const SizedBox(
+                                width: 0,
+                                child: Icon(
+                                  Icons.keyboard_arrow_right,
+                                  color: Color(0xff999999),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: const Color(0xfff2f2f2),
+                              border: InputBorder.none,
+                              hintText: '请输入',
+                              hintStyle: const TextStyle(
+                                  fontSize: 20, color: Color(0xff999999))),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return '不能为空';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const CodeBlock(
+                code: '''
+  InkWell(
+    onTap: () {
+      BrnToast.show('哈哈哈哈', context);
+    },
+    child: Container(
+      margin: const EdgeInsets.only(top: 1),
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: TextFormField(
+              textAlign: TextAlign.right,
+              enabled: false,
+              style: const TextStyle(fontSize: 20),
+              decoration: InputDecoration(
+                  prefixIcon: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10),
+                    width: 100,
+                    child: const Row(
+                      // mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          '姓名',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  suffixIcon: const SizedBox(
+                    width: 0,
+                    child: Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Color(0xff999999),
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xfff2f2f2),
+                  border: InputBorder.none,
+                  hintText: '请输入',
+                  hintStyle: const TextStyle(
+                      fontSize: 20, color: Color(0xff999999))),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return '不能为空';
+                }
+                return null;
+              },
+            ),
+          ),
+        ],
+      ),
+    ),
+  ),
+          ''',
+                title: '关键代码',
               ),
             ],
           ),
