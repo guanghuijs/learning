1cfa66b43ee84e544c6b10d606d1c2448f5c1efb
吴光辉 <995980020@qq.com>
2024-06-24 22:30:53 +0800

    feat(vue3): 加入环节变量

--- vue/vue3/vite.config.ts
+++ vue/vue3/vite.config.ts
@@ -1,28 +1,31 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-    Pages({
-      dirs: './src/views', //指定生成路由的目录
-      extensions: ['vue'], //文件后缀
-      exclude: ['**/src/*.vue'], //可以排除指定目录
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(() => {
+  const { GLOBAL_PORT } = loadEnv('', '', 'GLOBAL');
+  return {
+    base: './',
+    plugins: [
+      vue(),
+      vueJsx(),
+      Pages({
+        dirs: './src/views', //指定生成路由的目录
+        extensions: ['vue'], //文件后缀
+        exclude: ['**/src/*.vue'], //可以排除指定目录
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 8654,
-  },
+    server: {
+      port: Number(GLOBAL_PORT),
+    },
+  };
 });
