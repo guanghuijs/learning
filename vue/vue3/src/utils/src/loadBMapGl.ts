76e5cb5c625e4d5aee3a0b700d181e3db0a987dc
吴光辉 <995980020@qq.com>
2024-06-28 11:02:46 +0800

    feat(bmap): 百度地图(把ak放到环节变量里)

--- vue/vue3/src/utils/src/loadBMapGl.ts
+++ vue/vue3/src/utils/src/loadBMapGl.ts
@@ -1,19 +1,16 @@
 // @ts-nocheck
 export async function loadBMapGL() {
-  const AK = 'CIdrHeGtR84TzrwwBiXFrXnguaSZheAH';
-  const BMap_URL = `https://api.map.baidu.com/api?v=1.0&&type=webgl&ak=${AK}&callback=onBMapCallback`;
+  const { GLOBAL_BMAP_AK } = import.meta.env;
+  const BMap_URL = `https://api.map.baidu.com/api?v=1.0&&type=webgl&ak=${GLOBAL_BMAP_AK}&callback=onBMapCallback`;
 
   return new Promise((resolve) => {
     // 如果已加载直接返回
     if (typeof BMapGL !== 'undefined' && BMapGL !== null) {
-      console.log('百度地图脚本已加载...');
       resolve(BMapGL);
       return;
     }
 
     window.onBMapCallback = function () {
-      console.log('百度地图脚本初始化成功...');
-      console.log(BMapGL);
       resolve(BMapGL);
     };
 
