c53d956dc1fc95430dd9718ea93ec2eb1822d6b6
吴光辉 <995980020@qq.com>
2024-06-28 11:12:50 +0800

    feat(bmap): 百度地图(通过.then渲染地图)

--- vue/vue3/src/views/bmap/7.根据经纬度获取位置信息.vue
+++ vue/vue3/src/views/bmap/7.根据经纬度获取位置信息.vue
@@ -5,33 +5,34 @@
   const mapEle = ref<HTMLDivElement>();
   const map = ref<any>();
 
-  onMounted(async () => {
-    await initMap();
+  onMounted(() => {
+    initMap();
   });
 
   //初始化地图
-  async function initMap(coordinate: number[] = [102.850766, 24.892483]) {
-    const BMapGL: any = await loadBMapGL();
-    // 创建地图实例
-    map.value = new BMapGL.Map(mapEle.value, {
-      enableDblclickZoom: true,
-      enableMapClick: false,
-      displayOptions: {
-        building: false,
-      },
-      enableRotate: false,
-      enableTilt: false,
-    });
-    const point = new BMapGL.Point(coordinate[0], coordinate[1]);
-    map.value.centerAndZoom(point, 19);
-    map.value.clearOverlays();
-    map.value.addOverlay(new BMapGL.Marker(point));
-    map.value.enableScrollWheelZoom(true);
-    // 获取位置具体信息
-    const geoc = new BMapGL.Geocoder();
-    // point 是地图 Point 类
-    geoc.getLocation(point, (res: any) => {
-      console.log(res);
+  function initMap(coordinate: number[] = [102.850766, 24.892483]) {
+    loadBMapGL().then((BMapGL: any) => {
+      // 创建地图实例
+      map.value = new BMapGL.Map(mapEle.value, {
+        enableDblclickZoom: true,
+        enableMapClick: false,
+        displayOptions: {
+          building: false,
+        },
+        enableRotate: false,
+        enableTilt: false,
+      });
+      const point = new BMapGL.Point(coordinate[0], coordinate[1]);
+      map.value.centerAndZoom(point, 19);
+      map.value.clearOverlays();
+      map.value.addOverlay(new BMapGL.Marker(point));
+      map.value.enableScrollWheelZoom(true);
+      // 获取位置具体信息
+      const geoc = new BMapGL.Geocoder();
+      // point 是地图 Point 类
+      geoc.getLocation(point, (res: any) => {
+        console.log(res);
+      });
     });
   }
 </script>
