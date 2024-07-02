f0cffe11964a4a571f6935f3d962aabf6c8919c0
吴光辉 <995980020@qq.com>
2024-07-02 16:49:42 +0800

    fix(vue3): css背景网格

---
+++ vue/vue3/src/views/less/10.gridding.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { useSysStoreRefs } from '@/stores/sys';
+  const { primaryColor } = useSysStoreRefs();
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped lang="less">
+  .container {
+    height: 100%;
+    background-color: v-bind(primaryColor);
+    background-image: linear-gradient(
+        to right,
+        #1c3434 1px,
+        transparent 1px,
+        transparent 10px
+      ),
+      linear-gradient(to bottom, #1c3434 1px, transparent 1px, transparent 10px);
+    background-size: 20px 20px;
+  }
+</style>
