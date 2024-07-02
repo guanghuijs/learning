2a5ce211443641c82db2bffcf8d3daeea657435f
吴光辉 <995980020@qq.com>
2024-07-02 16:49:22 +0800

    fix(vue3): 刷短视频初版

---
+++ vue/vue3/src/views/demo/8.short_video.vue
@@ -0,0 +1,64 @@
+<template>
+  <swiper
+    class="mySwiper"
+    direction="vertical"
+    @swiper="onSwiper"
+    @slideChangeTransitionEnd="transitionEnd"
+    @slideChange="slideChange"
+  >
+    <swiper-slide
+      v-for="video in [CAT_1, CAT_2, CAT_3, CAT_4, CAT_5]"
+      class="swiper-item"
+    >
+      <video loop>
+        <source :src="video" />
+      </video>
+    </swiper-slide>
+  </swiper>
+</template>
+<script setup lang="ts">
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  // Import Swiper styles
+  import 'swiper/css';
+  import CAT_1 from '@/assets/video/cat_1.mp4';
+  import CAT_2 from '@/assets/video/cat_2.mp4';
+  import CAT_3 from '@/assets/video/cat_3.mp4';
+  import CAT_4 from '@/assets/video/cat_4.mp4';
+  import CAT_5 from '@/assets/video/cat_5.mp4';
+  import { onMounted } from 'vue';
+
+  onMounted(() => {
+    const video = document
+      .querySelector('.swiper-slide-active')!
+      .querySelector('video');
+    video!.play();
+  });
+
+  const onSwiper = () => {};
+  const transitionEnd = (res: any) => {
+    const video = document
+      .querySelector('.swiper-slide-active')!
+      .querySelector('video');
+    video!.play();
+  };
+
+  const slideChange = () => {
+    const video = document
+      .querySelector('.swiper-slide-active')!
+      .querySelector('video');
+    video!.pause();
+  };
+</script>
+<style lang="less" scoped>
+  .mySwiper {
+    height: 100%;
+    background: #333;
+    .swiper-item {
+      video {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+</style>
