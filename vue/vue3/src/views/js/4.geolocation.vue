fc90109434997701e4c5e3ee53ef442313d0f11b
吴光辉 <995980020@qq.com>
2024-09-25 18:55:58 +0800

    feat(vue3): 浏览器获取当前定位

---
+++ vue/vue3/src/views/js/4.geolocation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+
+  const p = ref();
+
+  onMounted(() => {
+    if ('geolocation' in navigator) {
+      // 浏览器支持 Geolocation
+      navigator.geolocation.getCurrentPosition((res) => {
+        console.log(res.coords);
+        p.value = res;
+      });
+    } else {
+      // 浏览器不支持 Geolocation
+    }
+  });
+</script>
+
+<template>{{ p.coords }}</template>
+
+<style scoped lang="less"></style>
