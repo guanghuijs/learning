d2a627f87186072ee25e4e7809e9d9dfa5e34113
吴光辉 <995980020@qq.com>
2024-12-18 16:45:25 +0800

    feat(vite): 配置学习

--- vite/config/vite.config.ts
+++ vite/config/vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig(({ command, mode }) => {
   console.log(import.meta.url);
   console.log(
     '\x1b[31m%s\x1b[0m',
-    '\n当前处于开发测试阶段，还会有大量更新，仅供参考，请勿用于实际项目！\n'
+    '\n当前处于开发测试阶段，还会有大量更新，仅供参考，请勿用于实际项目！\n',
   );
-  // 执行vite指令时,command维serve,mode默认为development;
-  // 执行build指令是command为build,mode默认为build;
+  // 执行vite指令时,command为serve,mode默认为development;
+  // 执行build指令是command为build,mode默认为production;
   // 执行vite、build指令时,mode也可以指定其他值;
   const env = loadEnv(mode, './env', ['VITE', 'DEV']);
   console.log(env);
