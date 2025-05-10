47dc92d36b46813cf7e4d27c17275a0e6deaa6f1
ghui <995980020@qq.com>
2025-05-10 17:40:22 +0800

    feat(uni-app): 同花顺基金页面效果

---
+++ vue/vue3/src/views/css/23.sequence.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+
+  const flag = ref(true);
+
+  onMounted(() => {
+    const dom = document.querySelector('.layout-main');
+
+    dom?.addEventListener('scroll', (e) => {
+      console.log(dom?.scrollTop);
+      flag.value = dom?.scrollTop <= 100;
+    });
+  });
+</script>
+
+<template>
+  <div class="top">
+    <div style="background: #0a98d5"></div>
+    <div v-show="flag" style="background: #d50a0a"></div>
+    <div style="background: #daa10e"></div>
+  </div>
+  <div class="content">
+    <div v-for="i in 50" :key="i">{{ i }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .top {
+    position: sticky;
+    top: 0;
+    div {
+      height: 50px;
+    }
+  }
+
+  .content {
+    height: 1000px;
+  }
+</style>
