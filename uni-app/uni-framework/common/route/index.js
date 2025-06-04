425fca16bce85d44f78f34da0af939abb63b853a
ghui <995980020@qq.com>
2025-06-04 10:10:39 +0800

    feat(uni-app): 删除不需要页面

--- uni-app/uni-framework/common/route/index.js
+++ uni-app/uni-framework/common/route/index.js
@@ -1,24 +1,34 @@
-export default [{
+export default [
+	{
 		key: 'demo',
 		title: 'demo',
 	},
 	{
 		key: 'location',
 		title: '定位',
-	}, {
+	},
+	{
 		key: 'drag_btn',
 		title: '拖拽按钮',
-	}, {
+	},
+	{
 		key: 'scroll_view',
 		title: 'scroll view',
-	}, {
+	},
+	{
 		key: 'scroll_view_2',
 		title: 'scroll view 2',
-	}, {
+	},
+	{
 		key: 'store',
 		title: 'store',
-	}, {
+	},
+	{
 		key: 'store_2',
 		title: 'store_2',
-	}
+	},
+	{
+		key: 'tabs-nav',
+		title: 'tabs-nav',
+	},
 ];
