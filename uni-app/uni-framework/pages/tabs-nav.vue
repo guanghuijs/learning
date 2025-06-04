a000ae7f55ef3ef45503714f3cfcaf84b4078ad5
ghui <995980020@qq.com>
2025-06-04 10:11:11 +0800

    feat(uni-app): tabs-nav

---
+++ uni-app/uni-framework/pages/tabs-nav.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="page flex-star">
+		<view class="nav">
+			<u-tabs :list="tabs" @click="tabChange"></u-tabs>
+		</view>
+		<view class="main">
+			<scroll-view
+				scroll-y="true"
+				:scroll-into-view="scrollToId"
+				scroll-with-animation
+			>
+				<view class="item" v-for="item in tabs" :key="item.id" :id="item.id">
+					<view class="content">
+						<view class="title">{{ item.name }}</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tabs: [
+					{ name: '详情', id: 'desc' },
+					{ name: '商铺', id: 'shop' },
+					{ name: '评论', id: 'comment' },
+					{ name: '发货', id: 'shipments' },
+				],
+				scrollToId: '',
+			};
+		},
+		components: {},
+		computed: {},
+		watch: {},
+		onLoad() {},
+		onShow() {},
+		methods: {
+			tabChange({ id }) {
+				this.$nextTick(() => {
+					this.scrollToId = id;
+				});
+			},
+		},
+		onReachBottom() {},
+		onPageScroll() {},
+		onUnload() {},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.page {
+		flex-direction: column;
+		height: 100vh;
+		.nav {
+			width: 100%;
+			background: white;
+			border-bottom: 1px solid #eee;
+		}
+
+		.main {
+			flex: 1;
+			width: 100%;
+			overflow-y: hidden;
+
+			scroll-view {
+				height: 100%;
+				padding-top: 40upx;
+
+				.item {
+					padding: 0 40upx;
+					margin-bottom: 40upx;
+					.content {
+						height: 50vh;
+						background: white;
+						width: 100%;
+					}
+				}
+			}
+		}
+	}
+</style>
