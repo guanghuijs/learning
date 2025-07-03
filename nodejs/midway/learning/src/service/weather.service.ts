bfa34ce278a05cda130d45843c65f86a7118e994
wuguanghui <995980020@qq.com>
2025-07-03 18:05:42 +0800

    feat(nodejs): midwayjs 学习

---
+++ nodejs/midway/learning/src/service/weather.service.ts
@@ -0,0 +1,18 @@
+import { Provide, makeHttpRequest } from '@midwayjs/core';
+import { WeatherInfo } from '../interface';
+
+@Provide()
+export class WeatherService {
+  async getWeather(cityId: string): Promise<WeatherInfo> {
+    const result = await makeHttpRequest<WeatherInfo>(
+      `https://midwayjs.org/resource/${cityId}.json`,
+      {
+        dataType: 'json',
+      }
+    );
+
+    if (result.status === 200) {
+      return result.data as WeatherInfo;
+    }
+  }
+}
